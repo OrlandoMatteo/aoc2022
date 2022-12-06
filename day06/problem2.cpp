fab19da2ec07e0d4b3386edec6ebe80db6910e1b
Matteo Orlando <matteo.orlando.1993@gmail.com>
2022-12-06 11:50:19 +0100

    day6

---
+++ day06/problem2.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <string.h>
+using namespace std;
+void print(list<char> const &vector) {
+  for (auto const &i : vector) {
+    std::cout << i;
+  }
+  std::cout << "" << endl;
+}
+char get(list<char> _list, int _i) {
+  list<char>::iterator it = _list.begin();
+  for (int i = 0; i < _i; i++) {
+    ++it;
+  }
+  return *it;
+}
+int main() {
+  // generate map to map each letter to its position
+
+  char c;
+  int lenght = 14;
+  list<char> last4;
+  fstream fin("input.txt", fstream::in);
+  for (int i = 0; i < 13; i++) {
+    fin >> c;
+    last4.push_back(c);
+  }
+  int position = 13;
+  int maxCombination = (14 * 13) / 2;
+  int different = 0;
+  while (fin >> c && different != maxCombination) {
+    different = 0;
+    last4.push_back(c);
+    for (int i = 0; i < lenght - 1; i++) {
+      for (int j = i + 1; j < lenght; j++) {
+        if (get(last4, i) != get(last4, j)) {
+          different++;
+        }
+      }
+    }
+    last4.pop_front();
+    last4.resize(13);
+    position++;
+  }
+  cout << position << endl;
+}
